777dbdabe6b008a2ca6d25f5d201c3eeb270ba2f
zempo <zelenkosolomon@gmail.com>
2021-02-16 18:42:17 -0800

    first commit, new branch

---
+++ src/main/java/com/solomonzelenko/getorders/GetordersApplication.java
@@ -0,0 +1,13 @@
+package com.solomonzelenko.getorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetordersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetordersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solomonzelenko/getorders/GetordersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solomonzelenko.getorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetordersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
